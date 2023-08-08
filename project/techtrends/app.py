de0f28143e76f2d56fe9d34a7598c33882d3e52c
Leticia <lepirescomp@gmail.com>
2023-08-08 18:00:50 +0100

    Add dockerfile

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -1,13 +1,27 @@
+import logging
 import sqlite3
+import sys
+
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
+
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(levelname)s:%(name)s:%(asctime)s, %(message)s', datefmt="%d/%m/%Y, %H:%M:%S")
+handler.setFormatter(formatter)
+log.addHandler(handler)
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    sqlite3
     return connection
 
 # Function to get a post using its ID
@@ -16,33 +30,53 @@ def get_post(post_id):
     post = connection.execute('SELECT * FROM posts WHERE id = ?',
                         (post_id,)).fetchone()
     connection.close()
+#    import pdb; pdb.set_trace()
     return post
 
 # Define the Flask application
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret key'
 
-# Define the main route of the web application 
-@app.route('/')
-def index():
+def get_posts():
     connection = get_db_connection()
     posts = connection.execute('SELECT * FROM posts').fetchall()
     connection.close()
+    return posts
+
+def create_post(title, content):
+    connection = get_db_connection()
+    try:
+        connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+                        (title, content))
+        connection.commit()
+        connection.close()
+        app.logger.info(f"Article {title} was added successfully.")
+    except sqlite3.Error:
+        pass
+    
+
+# Define the main route of the web application 
+@app.route('/')
+def index():
+    posts = get_posts()
     return render_template('index.html', posts=posts)
 
 # Define how each individual article is rendered 
 # If the post ID is not found a 404 page is shown
 @app.route('/<int:post_id>')
 def post(post_id):
-    post = get_post(post_id)
-    if post is None:
-      return render_template('404.html'), 404
+    article = get_post(post_id)
+    if article is None:
+        log.warning(f"Article not found!")
+        return render_template('404.html'), 404
     else:
-      return render_template('post.html', post=post)
+        log.info(f"Article {list(article)[2]} retrieved!")
+        return render_template('post.html', post=article)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    log.debug(f"About me was called.")
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -51,19 +85,39 @@ def create():
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
-
         if not title:
             flash('Title is required!')
         else:
-            connection = get_db_connection()
-            connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
-                         (title, content))
-            connection.commit()
-            connection.close()
-
+            create_post(title, content)
             return redirect(url_for('index'))
 
     return render_template('create.html')
+
+
+# Define the About Us page
+@app.route('/healthz')
+def healthz():
+    response = app.response_class(
+            response=json.dumps({"result":"OK - healthy"}),
+            status=200,
+            mimetype='application/json'
+    )
+    app.logger.info('Status request successfull')
+    app.logger.debug('DEBUG message')
+    return response
+
+
+@app.route('/metrics')
+def metrics():
+    posts = get_posts()
+    response = app.response_class(
+            response=json.dumps({"status":"success","code":0,"data":{"db_connection_count": 1, "post_count": len(posts)}}),
+            status=200,
+            mimetype='application/json'
+    )
+    app.logger.info('Metrics request successfull')
+    return response
+
 
 # start the application on port 3111
 if __name__ == "__main__":
